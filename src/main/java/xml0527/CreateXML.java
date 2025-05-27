651301cc1efb908e1798a3f8ce7648f5e8967143
tgncosist2 <tgncosist2@gmail.com>
2025-05-27 12:23:49 +0900

    createXML

--- src/main/java/xml0527/CreateXML.java
+++ src/main/java/xml0527/CreateXML.java
@@ -1,16 +1,45 @@
 package xml0527;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 public class CreateXML {
-	
-	public void createXML() {
-		Document doc = new Document();
+
+	public void createXML() throws IOException {
+		// 1. XML문서 객체 생성
+		Document doc = new Document(); // <?xml version="1.0" encoding="UTF-8" ?>
+
+		// 2. 최상위 부모노드 생성
+		Element rootNode = new Element("root"); // <root> </root>
+
+		// 3. 자식 노드를 생성
+		Element msgNode = new Element("msg");
+
+		// 3-1. 자식노드에 값 설정
+		msgNode.setText("안녕하세요?");
+
+		// 4. 배치
+		rootNode.addContent(msgNode);
+
+		// 4-1. XML문서객체에 최상위 부모노드 배치
+		doc.addContent(rootNode);
+
+		// 5. 생성된 XML을 출력, 출력객체 생성!
+		XMLOutputter xout = new XMLOutputter(Format.getPrettyFormat());
 		
-		System.out.println(doc);
-	}
+		xout.output(doc, System.out);
+	}// createXML
 
 	public static void main(String[] args) {
-		new CreateXML().createXML();
-	}
-}
+		try {
+			new CreateXML().createXML();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}// end try-catch
+	}// main
+}// class
